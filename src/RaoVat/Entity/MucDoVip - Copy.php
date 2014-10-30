d0158cf85af334d1b29a5218981bdf51611c26ba
luukimloan <luukimloan1991@gmail.com>
2014-10-30 09:35:06 +0700

    them MDV copy

---
+++ src/RaoVat/Entity/MucDoVip - Copy.php
@@ -0,0 +1,43 @@
+<?php
+ namespace RaoVat\Entity;
+
+ use Doctrine\ORM\Mapping as ORM;
+ use Doctrine\Common\Collection\ArrayCollection;
+ use Doctrine\Common\Collection\Collection;
+
+ /**
+ * @ORM\Entity
+ * @ORM\Table(name="muc_do_vip")
+ */
+ class MucDoVip{
+ 	/**
+	* @ORM\Column(name="id_muc_do_vip",type="integer")
+	* @ORM\Id
+	* @ORM\GeneratedValue
+	*/
+	private $idMucDoVip;
+
+	/**
+	* @ORM\Column(name="ten_muc_do_vip")
+	*/
+	private $tenMucDoVip;
+
+	public function setIdMucDoVip($idMucDoVip)
+	{
+		$this->idMucDoVip=$idMucDoVip;
+	}
+	public function getIdMucDoVip()
+	{
+		return $this->idMucDoVip;
+	}
+
+	public function setTenMucDoVip($tenMucDoVip)
+	{
+		$this->tenMucDoVip=$tenMucDoVip;
+	}
+	public function getTenMucDoVip()
+	{
+		return $this->tenMucDoVip;
+	}
+ }
+ ?>
