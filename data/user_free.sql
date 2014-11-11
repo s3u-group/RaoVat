c460a5e4554ae6f5547c75e3007ee87f3b335fcf
Luu Kim Loan <luukimloan1991@gmail.com>
2014-11-11 08:13:55 +0700

    xoa user_free.sql

--- data/user_free.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `user` (`user_id`, `username`, `email`, `display_name`, `password`, `state`, `city`, `birthday`) VALUES
-(0, 'freeuser', 'free@gmail.com', 'None', '$2y$14$MuYEf2FroHBQOs9wsIwDzONfZz81Kc5Lze7lqfhV.Pw9hqh5UW9ze', NULL, NULL, NULL);
-INSERT INTO `user_role_linker` (`user_id`, `role_id`) VALUES
-(0, 2);
